aa3dde4ed369f9619c030d83e0c6ccc32f9bbef7
RoGreat <64620440+RoGreat@users.noreply.github.com>
2025-11-12 04:49:12 +0000

    Update packages

--- pkgs/lenspect/default.nix
+++ pkgs/lenspect/default.nix
@@ -15,14 +15,14 @@
 
 python3Packages.buildPythonApplication rec {
   pname = "lenspect";
-  version = "1.0.1";
+  version = "1.0.2";
   pyproject = false;
 
   src = fetchFromGitHub {
     owner = "vmkspv";
     repo = "lenspect";
     tag = "v${version}";
-    hash = "sha256-p0QVQK4uengvqq/iyanqN9WT/zkgyL/q/gV4K91VA48=";
+    hash = "sha256-R3Y1t4qEXY7zuYZENzwiAoW1fDEN/YiatnFLPUOeRUE=";
   };
 
   nativeBuildInputs = [
